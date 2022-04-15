d21403e461a0d1dd1aea385133744a045866c434
Priyansh Maheshwari <priyanshmaheshwari94@gmail.com>
2022-04-15 13:42:55 +0530

    new code

--- index.js
+++ index.js
@@ -1,58 +1,68 @@
-const express = require("express")
-const bodyParser = require('body-parser')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-let port = 3000;
-const app = express()
-app.use(bodyParser.json());
-app.listen(port, () => {
-    console.log(`App running on ${port}, http://localhost:${port}`)
-})
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.status(200).json({"Status": "Working"}))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// const express = require("express")
+// const bodyParser = require('body-parser')
 
-app.get('/',(req, res) => {
-    res.status(200).json({
-        "status": "working"
-    })
-})
+// let port = 3000;
+// const app = express()
+// app.use(bodyParser.json());
+// app.listen(port, () => {
+//     console.log(`App running on ${port}, http://localhost:${port}`)
+// })
 
-app.post("/bfhl", (req, res) => {
-    console.log(req.body.data)
-    let arra = req.body.data
-    let num = [];
-    let alphabet =[];
-    // Change the user id as per user name and DOB
-    let user_id = 'shubham_goyal_10072001'
-    let email=`shubhamgoyal6230@gmail.com`
-    let rollNo = `0827IT191113`
-    // Check whether the given array contains numeric elements or not
-    let stat=false
-    arra.forEach((ele) => {
-        // Check if the number is even or odd
-        if(typeof(ele)==="number"){
-            console.log(`${ele} is number`)
-            num.push(ele)
-            stat=true
-        } else {
-            console.log(`${ele} is alphabet`)
-            alphabet.push(ele)
-            stat=true
-        }
+// app.get('/',(req, res) => {
+//     res.status(200).json({
+//         "status": "working"
+//     })
+// })
+
+// app.post("/bfhl", (req, res) => {
+//     console.log(req.body.data)
+//     let arra = req.body.data
+//     let num = [];
+//     let alphabet =[];
+//     // Change the user id as per user name and DOB
+//     let user_id = 'shubham_goyal_10072001'
+//     let email=`shubhamgoyal6230@gmail.com`
+//     let rollNo = `0827IT191113`
+//     // Check whether the given array contains numeric elements or not
+//     let stat=false
+//     arra.forEach((ele) => {
+//         // Check if the number is even or odd
+//         if(typeof(ele)==="number"){
+//             console.log(`${ele} is number`)
+//             num.push(ele)
+//             stat=true
+//         } else {
+//             console.log(`${ele} is alphabet`)
+//             alphabet.push(ele)
+//             stat=true
+//         }
         
 
-    })
-    if(stat){
-        res.status(200).json({
-            "is_success": stat,
-            "user_id": user_id,
-            "email": email,
-            "roll_number":rollNo,
-            "numbers": num,
-            "alphabets": alphabet
-        })
-    } else {
-        res.status(200).json({
-            "is_success": stat,
-            "user_id": user_id
-        })
+//     })
+//     if(stat){
+//         res.status(200).json({
+//             "is_success": stat,
+//             "user_id": user_id,
+//             "email": email,
+//             "roll_number":rollNo,
+//             "numbers": num,
+//             "alphabets": alphabet
+//         })
+//     } else {
+//         res.status(200).json({
+//             "is_success": stat,
+//             "user_id": user_id
+//         })
 
-    }
-})
+//     }
+// })
