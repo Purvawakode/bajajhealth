4f9885c408988eb2b29cc9867f29c57789dcb347
Vijendra <86598443+vijendranagar@users.noreply.github.com>
2022-04-15 14:01:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ express()
     let num = [];
     let alphabet =[];
     // Change the user id as per user name and DOB
-    let user_id = 'vijendra_nagar_02082001'
-    let email=`vijendranagarit19@acropolis,in`
+    let user_id = 'vijendra nagar_02082001'
+    let email=`vijendranagarit19@acropolis.in`
     let rollNo = `0827IT191129`
     // Check whether the given array contains numeric elements or not
     let stat=false
